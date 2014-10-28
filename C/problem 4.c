9390d14deb80c9671fb42fb4475f9fac97d03b81
harveyliu95 <liutongl5@gmail.com>
2014-10-28 17:59:20 +0800

    Create problem 4.c

---
+++ C/problem 4.c
@@ -0,0 +1,28 @@
+//passed 已通过
+#include <stdio.h>
+
+int fibonacci(int n);
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    printf("%d", fibonacci(N));
+    
+    return 0;
+}
+
+int fibonacci(int n)
+{
+   if (n == 0)
+   {
+       return 0;
+   }
+   else if (n == 1)
+   {
+       return 1;
+   }
+   else
+   {
+       return fibonacci(n-1) + fibonacci(n-2);
+   }
+}
